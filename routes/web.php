2337c8535d032358857ddfdad7d2778b91bfc092
joasmoura <40508788+joasmoura@users.noreply.github.com>
2021-10-25 23:37:06 -0300

    Atualizações

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::name('painel.')->middleware('auth')->prefix('painel')->group(function () {
